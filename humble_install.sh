d1b56547d1ece08db447d6649ed8355755e74182
Manyan3 <53809036+mi-kaneyon@users.noreply.github.com>
2024-02-19 11:10:50 +0900

    Added install shell for education project

---
+++ humble_install.sh
@@ -0,0 +1,32 @@
+
+# Prepare repository for humble
+
+locale  # check for UTF-8
+
+sudo apt update && sudo apt install locales
+sudo locale-gen en_US en_US.UTF-8
+sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+export LANG=en_US.UTF-8
+
+locale  # verify settings
+
+# add repository 
+
+sudo apt install software-properties-common
+sudo add-apt-repository universe
+
+# GPG key
+sudo apt update && sudo apt install curl -y
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+
+#key add sourcelist
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+
+# ROS2 humble Install Cartographer
+sudo apt update
+sudo apt upgrade -y
+
+sudo apt install ros-humble-desktop
+sudo apt install ros-dev-tools
+
+echo "source /opt/ros/humble/setup.bash" >> .bashrc
